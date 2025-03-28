f6664a18007ce21b06a5a4a5068954b964c12b7e
Jezebel1990 <jezebelguedes@gmail.com>
2025-03-28 02:15:04 -0300

    feat: Add home page

---
+++ src/views/Home/CustomHeader.vue
@@ -0,0 +1,7 @@
+<template>
+<h1 class="text-brand-main">Header</h1>
+</template>
+<script>
+export default {
+}
+</script>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,7 +1,40 @@
 <template>
-<h1 class="text-brand-main">Home</h1>
+  <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
+  <contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">
+      feedbacker © {{ currentYear }}
+    </p>
+  </div>
 </template>
+
 <script>
+import { onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
+
 export default {
+  components: { CustomHeader, Contact },
+  setup () {
+    const currentYear = ref(new Date().getFullYear())
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handleLogin () {}
+    function handleAccountCreate () {}
+
+    return {
+      handleLogin,
+      handleAccountCreate,
+      currentYear
+    }
+  }
 }
 </script>
